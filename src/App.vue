b444a050e3c83a0c1eac3db9e6769bb06671a2a2
zhumengyang <z1784311404@163.com>
2025-02-25 14:58:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- html -->
+<template>
+    <div class="app">
+        <h1>Hello World !</h1>
+    </div>
+</template>
+
+<!-- js/ts -->
+<script lang="ts">
+    export default {
+        name: 'App'
+    }
+</script>
+
+<!-- css -->
+<style>
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// 引入 createApp 用于创建应用
+import {createApp} from 'vue'
+// 引入 App 根组件
+import App from './App.vue'
+
+createApp(App).mount('#app')
