4eba2286558351957533fdada23174f4c830cff8
zhumengyang <z1784311404@163.com>
2025-02-26 15:48:01 +0800

    ref 和 reactive 对比

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,18 +1,12 @@
 <template>
     <div class="persion">
         <h2>汽车信息：一辆{{ car.brand }}车，价值：{{ car.price }} 个</h2>
+        <button @click="changeBrand">修改汽车品牌</button>
         <button @click="changePrice">修改汽车价格</button>
+        <button @click="changeCar">修改汽车</button>
         <br>
-        <h2>游戏列表：</h2>
-        <ul>
-            <li v-for="g in games" :key="g.id">
-                {{ g.name }}
-            </li>
-        </ul>
-        <button @click="changeFirstGame">修改第一个游戏的名字</button>
-        <hr>
-        <h2>测试：{{ obj.a.b.c }}</h2>
-        <button  @click="changeObj">测试</button>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum"> 点我加加</button>
     </div>
 
 </template>
@@ -23,32 +17,28 @@ export default {
 </script> -->
 
 <script lang="ts" setup name="Person">
-import { reactive } from 'vue'
-let car = reactive({ brand: '奔驰', price: 100 })
-let games = reactive([
-    { id: 'kjhasdkjahs001', name: 'lol' },
-    { id: 'kjhasdkjahs002', name: '原神' },
-    { id: 'kjhasdkjahs003', name: 'mdnf' }
-])
-let obj = reactive({
-    a: {
-        b: {
-            c: 666
-        }
-    }
-})
+import { reactive, ref } from 'vue'
+let car = ref({ brand: '奔驰', price: 100 })
+let sum = ref(0)
 
+
+
+function changeBrand() {
+    car.value.brand = 'BMW'
+}
 
 function changePrice() {
-    car.price += 10
+    car.value.price += 10
 }
 
-function changeFirstGame() {
-    games[0].name = '流星蝴蝶剑'
+function changeCar() {
+    // Object.assign(car, {brand:'奥迪',price:30})
+    
+    car.value = {brand:'奥迪',price:30}
 }
 
-function changeObj() {
-    obj.a.b.c += 33
+function changeSum() {
+    sum.value +=1
 }
 </script>
 
